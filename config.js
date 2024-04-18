af31ca758cb8f7057f54bc2d59b194125c41ec96
Noémie Lehuby <noemie.lehuby@zaclys.net>
2024-04-18 10:29:00 +0200

    Switch to OAuth 2.0
    https://github.com/openstreetmap/operations/issues/867

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var editorName = 'Sidewalker'
+var editorName = 'Luke Sidewalker'
 var version = '0.0.1'
 
 var useTestServer = false;
@@ -7,24 +7,31 @@ var urlOverpass = 'https://overpass-api.de/api/interpreter?data=';
 var urlJosm = 'http://127.0.0.1:8111/import?url=';
 var urlID = 'https://www.openstreetmap.org/edit?editor=id';
 
-var urlOsmTest = useTestServer
+var urlOsm = useTestServer
     ? 'https://master.apis.dev.openstreetmap.org'
     : 'https://www.openstreetmap.org';
 
 var redirectPath = window.location.origin + window.location.pathname;
+console.log(redirectPath)
 var auth = useTestServer
-    ? osmAuth({
-        url: urlOsmTest,
-        oauth_consumer_key: '',
-        oauth_secret: '',
-        auto: true,
-        //singlepage: true
-    })
-    : osmAuth({
-        url: urlOsmTest,
-        oauth_consumer_key: 'a9CFoAHkYhEZY5G42NjdqsHKXazAZoJ1jtVn0fBV',
-        oauth_secret: 'sTeC2rplH0RZtyQVl6t2Qtoy8bOU4EFbv8p8rP5x',
+    ? osmAuth.osmAuth({
+        url: urlOsm,
+        client_id: "luke_sidewalker",
         redirect_uri: redirectPath + "land.html",
-        auto: true,
-        //singlepage: true
+        scope: "read_prefs write_api",
+        singlepage: false
+    })
+    : osmAuth.osmAuth({
+        url: urlOsm,
+        client_id: "A5-wd3saMmeWsH__bVeSTP1Qv4AmMz5_BheZ7-7Fk8g",
+        redirect_uri: redirectPath + "land.html",
+        scope: "read_prefs write_api",
+        singlepage: false
     });
+
+if (window.location.search.slice(1).split('&').some(function(p) { return p.indexOf('code=') === 0; })) {
+    auth.authenticate(function() {
+        history.pushState({}, null, window.location.pathname);
+        checkOSMAuth();
+    });
+}
